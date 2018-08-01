6f95e85ef4906fbadf9af35ab348dea7d0d7a0c0
AGEA\lanacondio <leandro.anacondio@gmail.com>
2018-07-31 21:25:50 -0300

    first commiyt

---
+++ ConsoleTpTesis/ConsoleTpTesis/Models/Arc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTpTesis.Models
+{
+    public class Arc
+    {
+        public Node first { get; set; }
+        public Node second { get; set; }
+        public int Cost { get; set; }
+        public int Demand { get; set; }
+        public int Profit { get; set; }
+    }
+}
---
+++ ConsoleTpTesis/ConsoleTpTesis/Models/Truck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTpTesis.Models
+{
+    public class Truck
+    {
+        public int Id { get; set; }
+        public int Capacity { get; set; }
+        public int TimeLimit { get; set; }
+
+    }
+}
